a71403fbbdfb48433520800c1e3629ef230cf55e
Jessica Kellogg <jmy.kellogg@gmail.com>
2016-10-04 12:29:45 -0500

    added some form workign on answer

---
+++ businessServer/client/components/SingleForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const SingleForm = React.createClass({
+	addNewAnswer: function(e){
+		// {answers}.push(this.refs.answer.value)
+		console.log(this.refs.answer.value);
+		e.preventDefault();
+	},
+	addNewNode: function(e){
+		console.log('second');
+		e.preventDefault();
+
+	},
+	render: function(){
+		const options = [{name:"Yes/No", value:"Yes/No"}, {name:"Either", value:"Either"}, {name:"Or", value:"Or"}]; 
+		const repeatOption = options.map((item, i) => {
+			return (
+				<option key={i}>{item.name}</option>
+				)
+		});
+		// const answers= [];
+		// const repeatAnswer = answers.map((answer) => {
+		// 	return (
+	 //    			<p>Answer: {answer}</p>
+		// 		)
+		// });
+	    return (
+	    	<div>
+	    		<form>
+	    		<div>
+	    			<label htmlFor="type">Type: </label>
+	    			<select name="type">
+	    				{repeatOption}
+	    			</select>
+	    		</div>
+	    		<div>
+	    			<label htmlFor="question">Question: </label>
+	    			<textarea name="question"></textarea>
+	    		</div>
+	    		<div>
+	    			{/*repeatAnswer*/}
+	    		</div>
+	    		<div>
+	    			<label htmlFor="answer">Answer: </label>
+	    			<input ref="answer" name="answer"></input>
+	    			<button onClick={this.addNewAnswer}>Add Answer</button>	
+	    		</div>
+	    		<div>
+	    			<button onClick={this.addNewNode}>Add Node</button>
+	    		</div>
+	    		</form>
+	    	</div>
+	    )
+	}
+});
+
+export default SingleForm 
--- businessServer/client/stores/store.js
+++ businessServer/client/stores/store.js
@@ -2,20 +2,19 @@
 // import { syncHistoryWithStore} from 'react-router-redux';
 // import { browserHistory } from 'react-router';
 
-// import the root reducer
-// import rootReducer from './reducers/index';
+// // import the root reducer
+// // import rootReducer from './reducers/index';
 
-// import comments from './data/comments';
-// import posts from './data/posts';
+// // import comments from './data/comments';
+// // import posts from './data/posts';
 
 // // create an object for the default data
 // const defaultState = {
-//   posts,
-//   comments
+//   answers 
 // };
 
-// const store = createStore(rootReducer, defaultState);
+// //const store = createStore(rootReducer, defaultState);
 
-// export const history = syncHistoryWithStore(browserHistory, store);
+// // export const history = syncHistoryWithStore(browserHistory, store);
 
-// export default store;
+// export default defaultState;
