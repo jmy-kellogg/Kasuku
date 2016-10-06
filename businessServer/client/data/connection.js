597d21c78c3ae49a30f2ac5a49820834cb8ed248
Jessica Kellogg <jmy.kellogg@gmail.com>
2016-10-06 17:28:57 -0500

    layers have carasoul

--- businessServer/client/data/connection.js
+++ businessServer/client/data/connection.js
@@ -1,11 +1,17 @@
 const connection = [
+{"id":1,"answer":"coffee","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":1,"toId":2,"businessId":1},{"id":2,"answer":"caf","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":2,"toId":3,"businessId":1},
+{"id":3,"answer":"decaf","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":2,"toId":3,"businessId":1},{"id":4,"answer":"yes","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":2,"toId":3,"businessId":1},
+{"id":5,"answer":"no","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":2,"toId":3,"businessId":1},{"id":6,"answer":"yes","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":3,"toId":4,"businessId":1},
+{"id":7,"answer":"no","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":3,"toId":6,"businessId":1},{"id":8,"answer":"milk","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":4,"toId":5,"businessId":1},
+{"id":9,"answer":"cream","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":4,"toId":5,"businessId":1},{"id":10,"answer":"powder","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":4,"toId":5,"businessId":1},
+{"id":11,"answer":"1","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":5,"toId":6,"businessId":1},{"id":12,"answer":"2","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":5,"toId":6,"businessId":1},
+{"id":13,"answer":"3","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":5,"toId":6,"businessId":1},{"id":14,"answer":"yes","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":6,"toId":7,"businessId":1},
+{"id":15,"answer":"no","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":6,"toId":9,"businessId":1},{"id":16,"answer":"splenda","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":7,"toId":8,"businessId":1},
+{"id":17,"answer":"cane","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":7,"toId":8,"businessId":1},{"id":18,"answer":"agave","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":7,"toId":8,"businessId":1},
+{"id":19,"answer":"1","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":8,"toId":9,"businessId":1},{"id":20,"answer":"2","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":8,"toId":9,"businessId":1},
+{"id":21,"answer":"3","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":8,"toId":9,"businessId":1},{"id":22,"answer":"small","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":9,"toId":null,"businessId":1},
+{"id":23,"answer":"medium","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":9,"toId":null,"businessId":1},{"id":24,"answer":"large","createdAt":"2016-10-06T21:25:27.979Z","updatedAt":"2016-10-06T21:25:27.979Z","fromId":9,"toId":null,"businessId":1}
 
-	{"id":1,"answer":"coffee","createdAt":"2016-10-05T15:17:30.817Z","updatedAt":"2016-10-05T15:17:30.817Z","fromId":1,"toId":2,"businessId":1},
-	{"id":2,"answer":"caf","createdAt":"2016-10-05T15:17:30.817Z","updatedAt":"2016-10-05T15:17:30.817Z","fromId":2,"toId":3,"businessId":1},
-	{"id":3,"answer":"decaf","createdAt":"2016-10-05T15:17:30.817Z","updatedAt":"2016-10-05T15:17:30.817Z","fromId":2,"toId":3,"businessId":1},
-	{"id":4,"answer":"large","createdAt":"2016-10-05T15:17:30.817Z","updatedAt":"2016-10-05T15:17:30.817Z","fromId":3,"toId":4,"businessId":1},
-	{"id":5,"answer":"medium","createdAt":"2016-10-05T15:17:30.817Z","updatedAt":"2016-10-05T15:17:30.817Z","fromId":3,"toId":4,"businessId":1}
-
-];
+]
 
 export default connection;
