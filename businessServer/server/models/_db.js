64e29ba0d47a4e2f00daa9ce178a755272f802b5
Doug VonMoser <doug.vonmoser@gmail.com>
2016-10-14 11:33:12 -0500

    changed _db to heroku server

--- businessServer/server/models/_db.js
+++ businessServer/server/models/_db.js
@@ -1,7 +1,24 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://localhost:5432/chatterbot', {
-    logging: false
+var dbUrl
+
+// if (process.env.DATABASE_URL) {
+//   dbUrl = process.env.DATABASE_URL
+// } else {
+//   dbUrl = 'postgres://abrjhrggwxjqut:aL1ivszolwho7m4Ly4W2LUC44x@ec2-54-243-203-93.compute-1.amazonaws.com:5432/dj92utpsnn9vj'
+// }
+
+if (!process.env.DATABASE_URL) {
+  console.log("DATABASE_URL not defined");
+  process.exit(0);
+}
+
+var db = new Sequelize(process.env.DATABASE_URL, {
+  native: true,
+  logging: true,
+  dialectOptions: {
+    supportBigNumbers: true
+  }
 });
 
 module.exports = db;
