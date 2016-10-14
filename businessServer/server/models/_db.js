cf6dd0b0cd70122e882fa167314d0026598f5255
Doug VonMoser <doug.vonmoser@gmail.com>
2016-10-14 13:36:07 -0500

    updated dbUrl to allow master push

--- businessServer/server/models/_db.js
+++ businessServer/server/models/_db.js
@@ -1,24 +1,21 @@
 var Sequelize = require('sequelize');
 
-var dbUrl
+var dbUrl, db;
 
-// if (process.env.DATABASE_URL) {
-//   dbUrl = process.env.DATABASE_URL
-// } else {
-//   dbUrl = 'postgres://abrjhrggwxjqut:aL1ivszolwho7m4Ly4W2LUC44x@ec2-54-243-203-93.compute-1.amazonaws.com:5432/dj92utpsnn9vj'
-// }
-
-if (!process.env.DATABASE_URL) {
-  console.log("DATABASE_URL not defined");
-  process.exit(0);
+if (process.env.DATABASE_URL) {
+	db = new Sequelize(process.env.DATABASE_URL, {
+	  native: true,
+	  logging: true,
+	  dialectOptions: {
+	    supportBigNumbers: true
+	  }
+	});
+} 
+else {
+	db = new Sequelize('postgres://localhost:5432/chatterbot', {
+    	logging: false
+	});
 }
 
-var db = new Sequelize(process.env.DATABASE_URL, {
-  native: true,
-  logging: true,
-  dialectOptions: {
-    supportBigNumbers: true
-  }
-});
 
 module.exports = db;
