c0f4e9bb1d2d780b0250d51e21f1b8b23f54cd73
dejongnj <dejongnj@gmail.com>
2016-10-15 14:12:45 -0500

    fixed getting started button

--- businessServer/client/components/BusinessProfileGetStartedOption.js
+++ businessServer/client/components/BusinessProfileGetStartedOption.js
@@ -9,26 +9,22 @@ const BusinessProfileGetStartedOption = React.createClass({
       isOn: true
     }
   },
-
   getStartedOn(e) {
     e.preventDefault();
     this.setState({isOn: true});
-    console.log("getStartedOn");
     this.addGetStarted();
 
   },
   getStartedOff(e) {
     e.preventDefault();
     this.setState({isOn: false});
-    console.log("getStartedOff")
     this.removeGetStarted();
   },
 
   removeGetStarted () {
-    console.log("removeGetStarted", `/api/business/${this.props["data-id"]}`)
     axios.get(`/api/business/${this.props["data-id"]}`)
-    .then(function(val) {
-      console.log(val.data.pageToken);
+    .then( (val) => {
+     
       return fetch('https://graph.facebook.com/v2.6/me/thread_settings?access_token=' + val.data.pageToken, {
                 method: 'DELETE',
                 headers: {
@@ -41,11 +37,21 @@ const BusinessProfileGetStartedOption = React.createClass({
                 })
       })
     })
-    .then( (val) => { console.log(val) })
+    .then( (val) => { 
+      console.log(val.status)
+      return axios.put(`/api/business/${this.props["data-id"]}`, { getStartedButton: false })
+      }
+    )
+    .then( (_business) => {
+      this.setState({isOn: _business.data.getStartedButton})
+      console.log(this.state, "THIS WAS MY STATE");
+    })
   },
   addGetStarted () {
     axios.get(`/api/business/${this.props["data-id"]}`)
-    .then(function (val) {
+    .then( (val) => {
+      this.setState({isOn: val.data.getStartedButton})
+      console.log(this.state, "THIS WAS MY STATE");
       return fetch('https://graph.facebook.com/v2.6/me/thread_settings?access_token=' + val.data.pageToken, {
                 method: 'POST',
                 headers: {
@@ -61,24 +67,32 @@ const BusinessProfileGetStartedOption = React.createClass({
             })
       
     })
-    .then( (val) => { console.log(val) })
+    .then( (val) => { 
+      console.log(val.status) 
+      return axios.put(`/api/business/${this.props["data-id"]}`, { getStartedButton: true })
+    })
+    .then( (_business) => {
+      this.setState({isOn: _business.data.getStartedButton})
+      console.log(this.state, "THIS WAS MY STATE");
+    })
   },
 
   render (){
     const GETSTARTEDTOOLTIP=`This adds a "get started button for the first time engages in a chat`
-    console.log(this.props["data-id"])
+    console.log(this.state)
     return (
       <div>
         <form>
-          <label htmlFor="get-started-button">Include a "Get Started Button"?&nbsp;</label><TooltipGlyph tip={GETSTARTEDTOOLTIP}/>
+          <label htmlFor="get-started-button">Include a "Get Started Button":&nbsp;</label><TooltipGlyph tip={GETSTARTEDTOOLTIP}/>
           <div className="btn-group" data-toggle="buttons">
-            <label onClick={this.getStartedOn} className={ this.isOn ? 'btn btn-primary active' : 'btn btn-primary'}>
+            <label onClick={this.getStartedOn} className={ this.state.isOn ? 'btn btn-primary active' : 'btn btn-primary'}>
               <input type="radio" name="enable-get-started" id="enable-get-started" autoComplete="off" defaultChecked /> Enable Get Started Button
             </label>
-            <label onClick={this.getStartedOff} className={ !this.isOn ? 'btn btn-primary active' : 'btn btn-primary'}>
+            <label onClick={this.getStartedOff} className={ !this.state.isOn ? 'btn btn-primary active' : 'btn btn-primary'}>
               <input  type="radio" name="disable-get-started" id="disable-get-started" autoComplete="off" /> Disable Get Started Button
             </label>
           </div>
+          <h6>Currently you { this.state.isOn ? '' : 'do not'} have a "Get Started Button"</h6>
         </form>
       </div>
     )
