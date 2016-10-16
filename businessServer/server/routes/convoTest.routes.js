ee15c376d6283060ef607ffdb3f62b791140a435
Doug VonMoser <doug.vonmoser@gmail.com>
2016-10-16 11:43:31 -0500

    basic route for testing convo

--- businessServer/server/routes/convoTest.routes.js
+++ businessServer/server/routes/convoTest.routes.js
@@ -1,6 +1,8 @@
 var router = require('express').Router();
 module.exports = router;
 
+var getChatResponse = require('../convoTestLogic')
+
 var db = require('../models');
 var Node = db.model('node');
 var Connection = db.model('connection');
@@ -8,13 +10,16 @@ var Business = db.model('business');
 
 
 // req.body {
-// 	message : 'whatever they said'
+// 	input : 'hello mr. chatterbot sir'
 // }
 
 // return {
-// 	tellem: 'what they need to ' 
+// 	message: 'Welcome to coffee shop. What order can I get for you?' 
 // }
 
 router.post('/', function(req, res){
-
+	getChatResponse(req.body.input)
+	.then(message => {
+		res.json({message})
+	})
 })
